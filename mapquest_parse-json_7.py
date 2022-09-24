41213cdd03eb72f40f8337f4890f394d990a42a2
jamesanthonysing <jamesanthony.sing.cics@ust.edu.ph>
2022-09-24 08:48:04 +0000

    Added text about the program and error intros

--- mapquest_parse-json_7.py
+++ mapquest_parse-json_7.py
@@ -4,12 +4,21 @@ import requests
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
 key = "PJv71zxlt65ihzFpAKRuP6HQ3zaCJDQ9"
 
+print("========================================")
+print("MapQuest Pathfinder 2.0")
+print("Improved by Group#8 - 4-ITI")
+print("========================================")
+
 while True:
     orig = input("Starting Location: ")
     if orig == "quit" or orig == "q":
+        print("========================================")
+        print("Thank you for using MapQuest Pathfinder!")
         break
     dest = input("Destination: ")
     if dest == "quit" or dest == "q":
+        print("========================================")
+        print("Thank you for using MapQuest Pathfinder!")
         break
     
     url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
@@ -34,14 +43,17 @@ while True:
         print("========================================")
     elif json_status == 402:
         print("****************************************")
+        print("Oops! We encountered an error.")
         print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
         print("****************************************")
     elif json_status == 611:
         print("****************************************")
+        print("Oops! We encountered an error.")
         print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
         print("****************************************")
     else:
         print("****************************************")
+        print("Oops! We encountered an error.")
         print("For Status Code: " + str(json_status) + "; Refer to:")
         print("https://developer.mapquest.com/documentation/directions-api/status-codes")
         print("****************************************\n")
