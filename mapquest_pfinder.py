78ca09901d61eeb96b9d5b467702575bbc112e1e
jamesanthonysing <jamesanthony.sing.cics@ust.edu.ph>
2022-09-26 07:03:08 +0000

    Updated output with colorama

--- mapquest_pfinder.py
+++ mapquest_pfinder.py
@@ -1,14 +1,18 @@
 # import needed libraries
+from enum import auto
 import urllib.parse
 import requests
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
 
 # Menu definition
 def menu():
     print("----------------------------------------")
-    print("What would you like to know about \n"+ orig +" to "+ dest +"?")
-    print("[1] General Info | [2] Restrictions")
-    print("[3] Miscellaneous | [4] Routes")
-    print("[0] Exit Menu")
+    print(Fore.MAGENTA + "What would you like to know about \n"+ orig +" to "+ dest +"?")
+    print(Fore.BLUE +  "[1]" + Fore.WHITE + " General Info | " + Fore.BLUE +  "[2]" + Fore.WHITE +  " Restrictions")
+    print(Fore.BLUE +  "[3]" + Fore.WHITE + " Miscellaneous | " + Fore.BLUE +  "[4]" + Fore.WHITE +  " Routes")
+    print(Fore.RED + "[0]" + Fore.WHITE + " Exit Menu")
     print("----------------------------------------")
 
 # Import MapQuest API
@@ -16,18 +20,18 @@ main_api = "https://www.mapquestapi.com/directions/v2/route?"
 key = "PJv71zxlt65ihzFpAKRuP6HQ3zaCJDQ9"
 
 print("========================================")
-print("MapQuest Pathfinder")
-print("Improved by Group#8 - 4-ITI")
+print(Fore.RED + "         " + "MapQuest Pathfinder")
+print(Fore.CYAN + "     " + "Improved by Group#8 - 4-ITI")
 print("========================================")
 
 while True:
     # Ask user for input regarding starting location & destination
-    orig = input("Starting Location: ")
+    orig = input(Style.BRIGHT + "Starting Location: ")
     if orig == "quit" or orig == "q":
         print("========================================")
         print("Thank you for using MapQuest Pathfinder!")
         break
-    dest = input("Destination: ")
+    dest = input(Style.BRIGHT + "Destination: ")
     if dest == "quit" or dest == "q":
         print("========================================")
         print("Thank you for using MapQuest Pathfinder!")
@@ -54,16 +58,16 @@ while True:
     
         while loop == 1:
             # Ask user for option number from menu
-            option = int(input("Please enter your option: "))
+            option = (input(Fore.CYAN +"Please enter your option: "))
 
-            if option == 1:
+            if option == "1":
                 print("SELECTED: General Info")
                 print("Trip Duration:   " + (json_data["route"]["formattedTime"]))
                 print("Kilometers:      " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
                 print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
                 
                 menu()
-            elif option == 2:
+            elif option == "2":
                 print("SELECTED: Restrictions")
                 print("Access restriction: " + str(json_data["route"]["hasAccessRestriction"]))
                 print("Timed restriction: " + str(json_data["route"]["hasSeasonalClosure"]))
@@ -71,7 +75,7 @@ while True:
                 print("Country cross: " + str(json_data["route"]["hasCountryCross"]))
                 
                 menu()
-            elif option == 3:
+            elif option == "3":
                 print("SELECTED: Miscellaneous")
                 print("Has toll road: " + str(json_data["route"]["hasTollRoad"]))
                 print("Has tunnel: " + str(json_data["route"]["hasTunnel"]))
@@ -82,44 +86,44 @@ while True:
                 print("Geo Quality Code of "+ dest + ": " + (json_data["route"]["locations"][1]["geocodeQualityCode"]))
 
                 menu()
-            elif option == 4:
+            elif option == "4":
                 print("SELECTED: Routes")
                 for each in json_data["route"]["legs"][0]["maneuvers"]:
                     print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
                 
                 menu()
-            elif option == 0:
+            elif option == "0":
                 loop = 0
-                print("Thank you for using the menu.")
+                print(Fore.GREEN + "Thank you for using the menu.")
                 print("----------------------------------------")
             else:
-                print("Oops! Invalid option. Please choose a number from the menu.")
+                print(Fore.RED + "Oops! Invalid option. Please choose a number from the menu.")
                 menu()
 
     # Output if unsuccessful json route calls
     elif json_status == 402:
         print("****************************************")
-        print("Oops! We encountered an error.")
-        print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
+        print(Fore.RED + "Oops! We encountered an error.")
+        print(Fore.RED + "Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
         print("****************************************")
     elif json_status == 611:
         print("****************************************")
-        print("Oops! We encountered an error.")
-        print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
+        print(Fore.RED + "Oops! We encountered an error.")
+        print(Fore.RED + "Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
         print("****************************************")
     elif json_status == 500: # Added status code 500
         print("****************************************")
-        print("Oops! We encountered an error.")
-        print("Status Code: " + str(json_status) + "; The server encountered an error and could not complete your request.")
+        print(Fore.RED + "Oops! We encountered an error.")
+        print(Fore.RED + "Status Code: " + str(json_status) + "; The server encountered an error and could not complete your request.")
         print("****************************************")
     elif json_status == 404: # Added status code 404
         print("****************************************")
-        print("Oops! We encountered an error.")
-        print("Status Code: " + str(json_status) + "; The resource addressed by the request URL does not exist.")
+        print(Fore.RED + "Oops! We encountered an error.")
+        print(Fore.RED + "Status Code: " + str(json_status) + "; The resource addressed by the request URL does not exist.")
         print("****************************************")
     else:
         print("****************************************")
-        print("Oops! We encountered an error.")
-        print("For Status Code: " + str(json_status) + "; Refer to:")
-        print("https://developer.mapquest.com/documentation/directions-api/status-codes")
+        print(Fore.RED + "Oops! We encountered an error.")
+        print(Fore.RED + "For Status Code: " + str(json_status) + "; Refer to:")
+        print(Fore.RED + "https://developer.mapquest.com/documentation/directions-api/status-codes")
         print("****************************************\n")
